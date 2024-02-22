3d9145b7e7e8bdd344bdf332142e312c7c2a4352
Deepakvrma14 <deepakverma1482003@gmail.com>
2024-02-22 11:09:19 +0530

    removing clutter

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,15 +3,13 @@ import Router from "./routes";
 // theme
 import ThemeProvider from './theme';
 // components
-import ThemeSettings from './components/settings';
+
 
 function App() {
   return (
     <ThemeProvider>
-      <ThemeSettings>
         {" "}
         <Router />{" "}
-      </ThemeSettings>
     </ThemeProvider>
   );
 }
