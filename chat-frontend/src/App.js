4d24a3deb37f0f6054587100747aa9f9f988dd12
Deepakvrma14 <deepakverma1482003@gmail.com>
2024-03-15 18:31:04 +0530

    snakcbar reducers done

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,27 +1,23 @@
 // routes
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 import Router from "./routes";
 // theme
 import ThemeProvider from "./theme";
+import { useSelector } from "react-redux";
 // components
 
 function App() {
+  const {open, severity, message} = useSelector((state)=> state.appState.snackbar);
   return (
     <>
       <ThemeProvider>
         <Router />
       </ThemeProvider>
-      <Snackbar
-        anchorOrigin={{ vertical, horizontal }}
-        open={open}
-        autoHideDuration={5000}
-        key={vertical + horizontal}
-      >
-        <Alert severity={severity} sx={{width:"100%"}} >
-        {message}
+      <Snackbar open={open} autoHideDuration={5000}>
+        <Alert severity={severity} sx={{ width: "100%" }}>
+          {message}
         </Alert>
-        
       </Snackbar>
     </>
   );
--- chat-frontend/src/app/features/appSlice.js
+++ chat-frontend/src/app/features/appSlice.js
@@ -1,35 +1,52 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-const defaultState= {
-    sidebar: {
-        open:false,
-        type: 'CONTACT'
-    },
-    snackbar: {
-        open:null,
-        message: null,
-        severity:null,
-    }
+const defaultState = {
+  sidebar: {
+    open: false,
+    type: "CONTACT",
+  },
+  snackbar: {
+    open: null,
+    message: null,
+    severity: null,
+  },
 };
 const appSlice = createSlice({
-    
-    name: 'app' ,
-    initialState:defaultState,
-    reducers:{
-        // toggle Sidebar
-        toggleSidebar(state){
-            
-            state.sidebar.open = !state.sidebar.open ;
-
-        },
-        updateSidebarType(state, action){
-            state.sidebar.type = action.payload.type;
-
-        },
+  name: "app",
+  initialState: defaultState,
+  reducers: {
+    // toggle Sidebar
+    toggleSidebar(state) {
+      state.sidebar.open = !state.sidebar.open;
     },
+    updateSidebarType(state, action) {
+      state.sidebar.type = action.payload.type;
+    },
+    openSnackbar(state, action) {
+      state.snackbar.open = true;
+      state.snackbar.severity = action.payload.severity;
+      state.snackbar.message = action.payload.message;
+    },
+    closeSnackbar(state, action) {
+      state.snackbar.open = false;
+      state.snackbar.severity = null;
+      state.snackbar.message = null;
+    },
+  },
 });
-
 
 export const { toggleSidebar, updateSidebarType } = appSlice.actions;
 export default appSlice.reducer;
 
+export function showSnackbar({ severity, message }) {
+  return async (dispatch, getState) => {
+    dispatch(appSlice.actions.openSnackbar({ message, severity }));
+    setTimeout(() => {
+      dispatch(appSlice.actions.closeSnackbar());
+    }, 4000);
+  };
+}
+
+export const closeSnackbar = () => async (dispatch,getState) =>{
+    dispatch(appSlice.actions.closeSnackbar());
+}
