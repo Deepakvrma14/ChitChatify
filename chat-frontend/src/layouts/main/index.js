d0c5308ff5d2590da76fac2e3bc24e76bdabaee1
Deepakvrma14 <deepakverma1482003@gmail.com>
2024-04-16 15:37:00 +0530

    UI done

--- chat-frontend/src/layouts/main/index.js
+++ chat-frontend/src/layouts/main/index.js
@@ -1,21 +1,28 @@
 import { Container, Stack } from "@mui/material";
+import {useTheme } from "@mui/material/styles";
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import icon from '../../assets/Images/icon.png';
-import {  useSelector } from 'react-redux';
+import icon from "../../assets/Images/icon.png";
+import { useSelector } from "react-redux";
+import { PawPrint } from "phosphor-react";
 const MainLayout = () => {
-  
-  const {isLoggedIn} = useSelector((state)=> state.authState);
-  if(isLoggedIn){
-    return <Navigate to="/app"/>
+  const { isLoggedIn } = useSelector((state) => state.authState);
+  if (isLoggedIn) {
+    return <Navigate to="/app" />;
   }
+  const theme = useTheme();
   return (
     <>
-      <Container sx={{ mt: 5 }} maxWidth="sm"  >
-        <Stack spacing={5} >
-<Stack sx={{width:"100%" }} direction={"column"} alignItems={"center"}  >
-  <img src= {icon} height={100} width={130} />
-</Stack>
+      <Container sx={{ mt: 5 }} maxWidth="sm">
+        <Stack spacing={5}>
+          <Stack
+            sx={{ width: "100%"  }}
+            direction={"column"}
+            alignItems={"center"}
+          >
+            <PawPrint color={theme.palette.background.logo}  size={100} weight="fill" />
+            {/* <img src={icon} height={100} width={130} /> */}
+          </Stack>
         </Stack>
         {/* <div>Main Layout</div> */}
 
--- chat-frontend/src/pages/Page404.js
+++ chat-frontend/src/pages/Page404.js
@@ -1,8 +1,44 @@
+import { Error } from "@mui/icons-material";
+import { Button, Stack, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Repeat } from "phosphor-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Page404 = () => {
-  return <>404</>;
-};
+  const navigate = useNavigate();
+  const theme = useTheme();
 
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
+      sx={{
+        height: "100vh",
+        textAlign: "center",
+        p: 1,
+        width: "100%",
+      }}
+    >
+      <Error color={theme.palette.background.paper} style={{ fontSize: 100 }} />
+      <Typography variant="h3">
+        Oops! We got into some errors.
+      </Typography>
+      <Typography variant="subtitle1">
+        The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<Repeat />}
+        onClick={() => navigate("/app")}
+      >
+        Go to Home Page
+      </Button>
+    </Stack>
+  );
+};
 
 export default Page404;
